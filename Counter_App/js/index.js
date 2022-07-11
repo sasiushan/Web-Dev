b9af8d8fa1eb0631855ddce5a278b8b1cc018540
*Sasiru <*sasi.ushan@icloud.com*>
2022-07-11 11:20:35 +0530

    addded a save button and stack DSA to save each count

--- Counter_App/js/index.js
+++ Counter_App/js/index.js
@@ -1,3 +1,4 @@
+
 function inc()
 {
     let x = document.getElementById("counter").innerHTML;
@@ -15,3 +16,47 @@ function dec()
     }    
     document.getElementById("counter").innerHTML = x;
 }
+
+function saveCount()
+{
+    let x = document.getElementById("counter").innerHTML;
+    x = parseInt(x);
+    stack.push(x);
+    document.getElementById("savedCount").innerHTML = stack.printStack();
+    
+}
+
+class Stack{
+    
+    constructor()
+    {
+        this.items = [];
+    }
+
+    push(input)
+    {
+        this.items.push(input)
+    }
+
+    pop()
+    {
+        if(this.items.length==0)
+        {
+            return 0;
+        }
+        else{
+            return this.items.pop();
+        }
+    }
+
+    printStack()
+    {
+        var str = "";
+        for(var i=0;i<this.items.length;i++)
+        {
+            str= str + this.items[i] + " ";
+        }
+        return str;
+    }
+}
+var stack = new Stack();
